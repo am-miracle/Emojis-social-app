9e02c4c0e528d323eb152987579053916c09ac70
Miracle Jude <judemiraco0@gmail.com>
2023-04-04 15:00:01 +0100

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
 
+  // console.log(data)
   return (
     <>
       <Head>
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!user.isSignedIn && <SignInButton />}{!!user.isSignedIn && <SignOutButton />}</div>
         {/* <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" /> */}
+        <div>{data?.map(post => <div key={post.id}>{post.content}</div>)}</div>
       </main>
     </>
   );
