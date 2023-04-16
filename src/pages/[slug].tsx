2096c369ca92a2046ad39af2e4d45b6e36f89d63
Miracle Jude <judemiraco0@gmail.com>
2023-04-16 14:20:19 +0100

    ssg helpers

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,13 +2,10 @@ import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import Loading from '~/components/Loading'
 import { api } from '~/utils/api';
-import { createProxySSGHelpers } from '@trpc/react-query/ssg';
-import { prisma } from "~/server/db"
-import { appRouter } from '~/server/api/root';
-import superjson from 'superjson';
 import Layout from '~/components/Layout';
 import Image from 'next/image';
 import PostView from '~/components/PostView';
+import { generateSSGHelper } from '~/server/helpers/ssgHelpers';
 
 
 const ProfileFeed = (props: { userId: string}) => {
@@ -63,11 +60,7 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = createProxySSGHelpers({
-    router: appRouter,
-    ctx: { prisma, userId: null },
-    transformer: superjson, // optional - adds superjson serialization
-  });
+  const ssg = generateSSGHelper();
 
   const slug = context.params?.slug
 
---
+++ src/server/helpers/ssgHelpers.ts
@@ -0,0 +1,15 @@
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { appRouter } from "../api/root";
+import superjson from 'superjson';
+import { prisma } from "~/server/db";
+
+
+
+
+
+export const generateSSGHelper = () =>
+    createProxySSGHelpers({
+      router: appRouter,
+      ctx: { prisma, userId: null },
+      transformer: superjson, // optional - adds superjson serialization
+});
